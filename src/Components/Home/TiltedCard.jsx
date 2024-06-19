b62236bd502211c85fe5c26b81b24c04af39af86
Rishikeshari7 <rishikeshari7@gmail.com>
2024-06-19 15:56:25 +0530

    homepage -> tilted card section , become an instructor section

---
+++ src/Components/Home/TiltedCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import BlueText from '../Common/BlueText'
+import img1 from "../../assets/Images/Know_your_progress.png"
+import img2 from "../../assets/Images/Compare_with_others.png"
+import img3 from "../../assets/Images/Plan_your_lessons.png"
+import "../../index.css"
+
+const TiltedCard = () => {
+  return (
+    <div className='flex w-10/12  flex-col justify-center items-center text-center'>
+    <div className='flex w-[75%]  items-center justify-center flex-col font-semibold gap-5'>
+        <p className='flex flex-col lg:flex-row text-richblack-900 text-4xl gap-2 '>Your swiss knife for <BlueText text={"learning any language"}/> </p>
+        <p className=' text-richblack-700'>Using spin making learning multiple languages easy. with 20+ languages realistic voice-over, progress tracking, custom schedule and more.</p>
+    </div>
+    <div className='flex flex-col justify-start items-center mt-5 lg:flex-row animate-float'>
+        <img className='w-[25rem] ' src={img1}></img>
+        <img className='w-[25rem] lg:mx-[-6rem]' src={img2}></img>
+        <img className='w-[25rem] mt-[-3rem] ml-[-1rem]' src={img3}></img>
+    </div>
+    </div>
+  )
+}
+
+export default TiltedCard
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,36 +1,37 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import BlueText from "../Components/Common/BlueText";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import CTAButton from "../Components/Home/CTAButton";
 import BannerVideo from "../assets/banner copy.mp4";
 import CodeBlock from "../Components/Home/CodeBlock";
-import "../index.css"
+import "../index.css";
 import TappingBar from "../Components/Home/TappingBar";
 import TappingCard from "../Components/Home/TappingCard";
-import {HomePageExplore} from "../Data/homepage-explore"
+import { HomePageExplore } from "../Data/homepage-explore";
 import TimeLineQuality from "../Components/Home/TimeLineQuality";
-import TimeLineImage from "../assets/Images/TimelineImage.png"
-
+import TimeLineImage from "../assets/Images/TimelineImage.png";
+import TiltedCard from "../Components/Home/TiltedCard";
+import Instructor from "../assets/Images/Instructor.png"
 
 const HomePage = () => {
-  const [selected , setSelected]=useState(HomePageExplore[0])
-  useEffect(()=>{
-    console.log("homePage ,selected ",selected)
-  },[])
+  const [selected, setSelected] = useState(HomePageExplore[0]);
+  useEffect(() => {
+    console.log("homePage ,selected ", selected);
+  }, []);
   return (
     <>
       <div className="bg-richblack-900">
         <div className="flex justify-center items-center flex-col gap-5 pt-14 pb-10">
           <NavLink to="/signup">
             <button className="flex items-center text-richblack-200 bg-richblack-800 rounded-full px-5 py-2 shadow-sm shadow-richblack-100 hover:bg-richblack-700 hover:text-white hover:scale-95 transition-all duration-300">
-              <p>Become an Instructor</p> <IoIosArrowRoundForward className="text-2xl"/>
+              <p>Become an Instructor</p>{" "}
+              <IoIosArrowRoundForward className="text-2xl" />
             </button>
           </NavLink>
           <div className="flex flex-col items-center  w-[55%] mx-auto">
             <p className="flex flex-col gap-[0.5rem] text-[2.25rem] font-semibold text-richblack-5 lg:flex-row">
-              Empower Your Future with{" "}
-              <BlueText text={"Coding Skills"}/>{" "}
+              Empower Your Future with <BlueText text={"Coding Skills"} />{" "}
             </p>
             <p className="text-richblack-200 mt-3 lg:text-center">
               {" "}
@@ -60,56 +61,53 @@ const HomePage = () => {
           </div>
         </div>
         <div>
-                    {/* {CodeBLOCK TYPEwriter 1} */}
+          {/* {CodeBLOCK TYPEwriter 1} */}
           <CodeBlock
-          position={"lg:flex-row"}
+            position={"lg:flex-row"}
             heading={
               <div className="text-4xl text-richblack-25 font-bold">
-                  Unlock your <BlueText
-                    text={`Coding potential`}
-                  ></BlueText> with our online courses.
+                Unlock your <BlueText text={`Coding potential`}></BlueText> with
+                our online courses.
               </div>
             }
             subHeading={
               "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
             }
             ctaBtn1={{
-                isActive:true,
-                path:"/signup",
-                title:"Try it Yourself"
+              isActive: true,
+              path: "/signup",
+              title: "Try it Yourself",
             }}
             ctaBtn2={{
-                isActive:false,
-                path:"/login",
-                title:"Learn More"
+              isActive: false,
+              path: "/login",
+              title: "Learn More",
             }}
             bgClr={<div className="codeblock1 absolute"></div>}
             color={"text-yellow-25"}
             codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
           ></CodeBlock>
 
-                              {/* {CodeBLOCK TYPEwriter 2} */}
+          {/* {CodeBLOCK TYPEwriter 2} */}
           <CodeBlock
-          position={"lg:flex-row-reverse"}
+            position={"lg:flex-row-reverse"}
             heading={
               <div className="text-4xl text-richblack-25 font-bold">
-                  Start <BlueText
-                    text={`coding in second`}
-                  ></BlueText>
+                Start <BlueText text={`coding in second`}></BlueText>
               </div>
             }
             subHeading={
               "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
-                 }
+            }
             ctaBtn1={{
-                isActive:true,
-                path:"/signup",
-                title:"Try it Yourself"
+              isActive: true,
+              path: "/signup",
+              title: "Try it Yourself",
             }}
             ctaBtn2={{
-                isActive:false,
-                path:"/login",
-                title:"Learn More"
+              isActive: false,
+              path: "/login",
+              title: "Learn More",
             }}
             color={"text-richblue-50"}
             gap={"14"}
@@ -117,53 +115,89 @@ const HomePage = () => {
             codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
           ></CodeBlock>
         </div>
-                      {/* {Tapping Bar Card section} */}
+        {/* {Tapping Bar Card section} */}
         <div className="flex justify-center items-center">
-            <TappingBar selected={selected} setSelected={setSelected}/>
+          <TappingBar selected={selected} setSelected={setSelected} />
         </div>
-                {/* white background start */}
-                {/* {TappingCard} */}
-                <div className="bg-pure-greys-5">
-                  <div className='flex flex-col mt-[-6rem] sm:mt-5  justify-center items-center bgHome' >
-                    <TappingCard selected={selected} />
-                    <div className="flex gap-5 mt-10 mb-20">
-                      <CTAButton isActive={true} path={"/signup"}>{"Explore Full Catalog"}<IoIosArrowRoundForward className="text-xl font-extrabold"/></CTAButton>
-                      <CTAButton isActive={false} path={"/login"}>{"Learn More"}</CTAButton>
+        {/* white background start */}
+        {/* {TappingCard} */}
+        <div className="bg-pure-greys-5">
+          <div className="flex flex-col mt-[-6rem] sm:mt-5  justify-center items-center bgHome">
+            <TappingCard selected={selected} />
+            <div className="flex gap-5 mt-10 mb-20">
+              <CTAButton isActive={true} path={"/signup"}>
+                {"Explore Full Catalog"}
+                <IoIosArrowRoundForward className="text-xl font-extrabold" />
+              </CTAButton>
+              <CTAButton isActive={false} path={"/login"}>
+                {"Learn More"}
+              </CTAButton>
+            </div>
+          </div>
+          <div className="h-[8rem] w-[100%] bg-richblack-900 "></div>
+          {/* QUALITY SECTION */}
+          <div className="flex justify-center items-center">
+            <div className="w-10/12">
+              <div className="flex flex-col md:flex-row justify-between gap-8 mt-10 mb-16 ">
+                <p className="text-richblack-900 text-4xl font-semibold gap-2 pt-2">
+                  Get the skills you need for a{" "}
+                  <BlueText text={" job that is in demand. "} />{" "}
+                </p>
+                <div className=" flex flex-col gap-5 w-[80%] md:w-[45%]">
+                  <p className="text-richblack-700 font-medium">
+                    The modern StudyNotion is the dictates its own terms. Today,
+                    to be a competitive specialist requires more than
+                    professional skills.
+                  </p>
+                  <CTAButton isActive={true} path={"/login"}>
+                    {"Learn More"}
+                  </CTAButton>
+                </div>
+              </div>
+              {/* QUALITY TimeLine */}
+              <div className="flex flex-col gap-14 mb-10 lg:flex-row lg:justify-around">
+                <div className="">
+                  <TimeLineQuality />
+                </div>
+                <div className=" relative shadow-[5px_8px_50px_2px] shadow-blue-200">
+                  <img
+                    className="lg:w-[38rem] object-contain  shadow-[15px_15px] shadow-white"
+                    src={TimeLineImage}
+                  ></img>
+                  <div className="absolute -bottom-12 left-14 text-white gap-5 flex justify-between p-3 md:p-5 w-[80%] bg-caribbeangreen-700">
+                    <div className="flex flex-col sm:flex-row justify-center items-start  sm:gap-3 md:w-[40%]">
+                      <p className=" text-4xl md:text-5xl font-bold">10</p>
+                      <p className="text-caribbeangreen-300 ">
+                        YEARS EXPERIENCES
+                      </p>
                     </div>
-                  </div>
-                  <div className="h-[8rem] w-[100%] bg-richblack-900 "></div>
-                                {/* QUALITY SECTION */}
-                  <div className="flex justify-center items-center">
-                    <div className="w-10/12"> 
-                        <div className="flex flex-col md:flex-row justify-between gap-8 my-16 ">
-                          <p className="text-richblack-900 text-4xl font-semibold gap-2 pt-2">Get the skills you need for a <BlueText text={" job that is in demand. "}/> </p>
-                          <div className=" flex flex-col gap-5 w-[80%] md:w-[45%]" >
-                            <p className="text-richblack-700 font-medium">The modern StudyNotion is the dictates its own terms. Today, to be a competitive specialist requires more than professional skills.</p>
-                            <CTAButton isActive={true} path={"/login"}>{"Learn More"}</CTAButton>
-                          </div>
-                        </div>
-                        {/* QUALITY TimeLine */}
-                        <div className="flex flex-col gap-14 mb-10 lg:flex-row lg:justify-around">
-                          <div className=""><TimeLineQuality/></div>
-                          <div className=" relative shadow-[5px_8px_50px_2px] shadow-blue-200">
-                            <img className="lg:w-[38rem] object-contain  shadow-[15px_15px] shadow-white" src={TimeLineImage}></img>
-                            <div className="absolute -bottom-12 left-14 text-white gap-5 flex justify-between p-5 w-[80%] bg-caribbeangreen-700">
-                              <div className="flex flex-col sm:flex-row justify-center items-start gap-3 w-[40%]">
-                                <p className="text-5xl font-bold">10</p>
-                                <p className="text-caribbeangreen-300 ">YEARS EXPERIENCES</p>
-                              </div>
-                              <div className=" my-2 w-[2px] border-l-[2px] border-caribbeangreen-500"></div>
-                              <div className="flex flex-col sm:flex-row justify-center items-start gap-3 w-[45%]">
-                                <p className="text-5xl font-bold">250</p>
-                                <p className="text-caribbeangreen-300 ">TYPES OF COURSES</p>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                    <div className=" my-2 w-[2px] border-l-[2px] border-caribbeangreen-500"></div>
+                    <div className="flex flex-col sm:flex-row justify-center items-  sm:gap-3 md:w-[45%]">
+                      <p className=" text-4xl md:text-5xl font-bold">250</p>
+                      <p className="text-caribbeangreen-300 ">
+                        TYPES OF COURSES
+                      </p>
                     </div>
-
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+
+          {/* TiltedCardSection */}
+          <div className="flex justify-center items-center py-20">
+            <TiltedCard />
+          </div>
+        </div>
+        {/* Become Instructor Section */}
+        <div className="flex flex-col lg:flex-row py-24 justify-evenly items-center gap-10">
+        <img className="w-[35rem] shadow-[-15px_-15px_rgb(255,255,255)] transition-all duration-500 hover:shadow-[15px_15px_rgb(255,255,255)]" src={Instructor}></img>
+        <div className="flex w-[35%] gap-4  flex-col">
+          <p className=" flex flex-col  text-richblack-5 text-4xl font-bold ">Become an <BlueText text={"instructor"}/> </p>
+          <p className="text-richblack-300 pb-2 font-semibold">Instructors from around the world teach millions of students on StudyNotion. We provide the tools and skills to teach what you love.</p>
+          <CTAButton isActive={"true"} path={"/signup"}>{"Start Teaching Today"} <IoIosArrowRoundForward className="text-2xl font-semibold"/> </CTAButton>
+        </div>
+        </div>
       </div>
     </>
   );
